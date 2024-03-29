eee7e99eec4188ac833d7d009152e6a1aeeb6d5a
Igor Bari <mucsi96@gmail.com>
2024-03-29 12:27:14 +0000

    fix style injection

--- client/src/core.ts
+++ client/src/core.ts
@@ -1,14 +1,20 @@
-import { LitElement } from "lit";
+import { CSSResult, LitElement } from 'lit';
 
 export class LightDOMLitElement extends LitElement {
   createRenderRoot() {
-    if (document.head.querySelector(`style[data-tagname="${this.tagName}"]`)) {
+    if (
+      document.head.querySelector(`style[data-tagname="${this.tagName}"]`) ||
+      !(
+        'styles' in this.constructor &&
+        this.constructor.styles instanceof CSSResult
+      )
+    ) {
       return this;
     }
 
-    const style = document.createElement("style");
-    style.innerHTML = `${this.tagName} {${this.style.cssText}}`;
-    style.setAttribute("data-tagname", this.tagName);
+    const style = document.createElement('style');
+    style.innerHTML = `${this.tagName} {${this.constructor.styles.cssText}}`;
+    style.setAttribute('data-tagname', this.tagName);
     document.head.append(style);
     return this;
   }
