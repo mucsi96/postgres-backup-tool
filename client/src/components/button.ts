9b8e7e02991dbad67806e38f2cdfa76c2f1c6f73
Igor Bari <mucsi96@gmail.com>
2024-04-01 12:50:36 +0000

    inheri font

--- client/src/components/button.ts
+++ client/src/components/button.ts
@@ -14,7 +14,7 @@ import { customElement } from "./utils";
       border-radius: 8px;
       color: white;
       font-weight: 500;
-      font-family: system-ui;
+      font-family: inherit;
       font-size: 14px;
       transition: background-color 0.3s;
 
--- client/src/components/inputLabel.ts
+++ client/src/components/inputLabel.ts
@@ -20,7 +20,7 @@ import { customElement } from './utils';
         color: white;
         font-size: 14px;
         font-weight: 400;
-        font-family: system-ui;
+        font-family: inherit;
         padding: 10px;
         border-radius: 8px;
         outline: none;
