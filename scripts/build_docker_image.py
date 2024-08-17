d921de8cea1ff476103897138f61ed48be41ccd1
Igor Bari <mucsi96@gmail.com>
2024-08-17 13:41:45 +0200

    fix publishing

--- scripts/build_docker_image.py
+++ scripts/build_docker_image.py
@@ -3,21 +3,24 @@
 from os import environ
 import sys
 from pathlib import Path
-from publish_tools import ansible_utils, docker_utils
+from publish_tools import ansible_utils, docker_utils, version_utils
 
 root_directory = Path(__file__).parent.parent
-secrets = ansible_utils.load_vars(sys.argv[2], root_directory / 'vars/vault.yaml')
-username = environ.get('GITHUB_REPOSITORY_OWNER')
+secrets = ansible_utils.load_vars(sys.argv[2], root_directory / "vars/vault.yaml")
+username = environ.get("GITHUB_REPOSITORY_OWNER")
 
 if username == None:
     print("GitHub username is missing", flush=True, file=sys.stderr)
     exit(1)
 
+version = version_utils.get_version(src=root_directory, tag_prefix="server")
+
 docker_utils.build_and_push_docker_img(
     src=root_directory,
+    version=version,
     tag_prefix="server",
     image_name="postgres-backup-tool",
     docker_username=username,
-    docker_password=secrets['docker_password'],
-    github_access_token=sys.argv[1]
+    docker_password=secrets["docker_password"],
+    github_access_token=sys.argv[1],
 )
